fda674063f41250788abb411e07aca020d218196
Kotori0629 <79943751@qq.com>
2022-12-10 20:20:22 +0800

    将玩家踩踏农作物与实体踩踏农作物分开处理判断
    允许白名单某世界允许踩踏农作物

--- src/main/java/cn/konfan/clearentity/listener/FarmProtectListener.java
+++ src/main/java/cn/konfan/clearentity/listener/FarmProtectListener.java
@@ -9,13 +9,17 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.List;
+
 public class FarmProtectListener implements Listener {
 
     private final Material soilMaterial = Material.getMaterial("SOIL") != null ? Material.getMaterial("SOIL") : Material.LEGACY_SOIL;
+    private final List<String> worldList = ClearEntity.getInstance().getConfig().getStringList("EntityManager.farm-protect-worlds");
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (!ClearEntity.getInstance().getConfig().getBoolean("EntityManager.enableFarmProtect")) return;
+        if (!ClearEntity.getInstance().getConfig().getBoolean("EntityManager.enablePlayerFarmProtect")) return;
+        if (!worldList.contains(event.getPlayer().getWorld().getName())) return;
         if (event.getAction() == Action.PHYSICAL) {
             if (event.getClickedBlock().getType() == soilMaterial) {
                 event.setCancelled(true);
@@ -25,7 +29,8 @@ public class FarmProtectListener implements Listener {
 
     @EventHandler
     public void onEntityInteract(EntityInteractEvent event) {
-        if (!ClearEntity.getInstance().getConfig().getBoolean("EntityManager.enableFarmProtect")) return;
+        if (!ClearEntity.getInstance().getConfig().getBoolean("EntityManager.enableEntityFarmProtect")) return;
+        if (!worldList.contains(event.getEntity().getWorld().getName())) return;
         if (!(event.getEntity() instanceof Player)) {
             if (event.getBlock().getType() == soilMaterial) {
                 event.setCancelled(true);
